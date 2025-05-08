d554a596a724b64595ceffac3b3081dde8c9a29a
ekflwk9 <ekflwk9@naver.com>
2025-05-08 09:53:15 +0900

    [feat] 메서드 사용 방법 Example을 보시면 됩니다.
    01.Scripts/Logic/Example에 메서드 사용 방법을 간단하게 작성했습니다 참고하실때 Example를 봐주시면 됩니다~

--- Assets/01.Scripts/Controller/Player.cs
+++ Assets/01.Scripts/Controller/Player.cs
@@ -7,43 +7,4 @@ public class Player : MonoBehaviour
         GameManager.SetComponent(this);
         DontDestroyOnLoad(this.gameObject);
     }
-
-    private void Example()
-    {
-        //컴포넌트 등록
-        GameManager.SetComponent(this);
-
-        //사운드 호출
-        GameManager.sound.OnEffect("PlayerHit");
-
-        //배경음 호출
-        GameManager.sound.OnMusic(null);
-
-        //이벤트 등록
-        GameManager.gameEvent.Add(Event);
-
-        //이벤트 호출
-        GameManager.gameEvent.Call("ObjectNameTestEventFunc");
-
-        //해당 오브젝트 => 하위 오브젝트 중 "Controller"라는 자식 오브젝트를 찾아옴
-        var child = Service.FindChild(this.transform, "Controller");
-
-        //Resources폴더 => Item 폴더 => Weapon이라는 파일을 불러옴
-        var resource = Service.FindResource("Item", "Weapon");
-    }
-
-    public void Event()
-    {
-        //페이드 인 => 페이드 인 종료시 이벤트 함수 호출
-        GameManager.fade.OnFade(EventFunc, 0.5f);
-    }
-
-    private void EventFunc()
-    {
-        //씬 전환시
-        GameManager.ChangeScene("NextSceneName");
-
-        //매개변수 메서드 없을 경우 페이드 아웃 재생
-        GameManager.fade.OnFade(0.5f);
-    }
 }
