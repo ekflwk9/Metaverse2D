b9c764fddb4326a49b889b88e0bc379577ab6767
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-05-08 20:38:33 +0900

    Add : MainButton
    StartButton, ExitButton 추가

---
+++ Assets/01.Scripts/Ui/BtnFunc.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BtnFunc : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private Image image;
+    [SerializeField] private Sprite defaultImage;
+    [SerializeField] private Sprite clickImage;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (image != null && clickImage != null)
+        {
+            image.sprite = clickImage;
+            
+        }
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (image != null && defaultImage != null)
+        {
+            image.sprite = defaultImage;
+        }
+    }
+
+
+    public void MainBtnAllOff()
+    {
+        gameObject.SetActive(false);
+    }
+}
