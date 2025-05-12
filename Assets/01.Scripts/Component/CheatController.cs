e6dc2f299779341b6ab92564322606c005ba107e
ekflwk9 <ekflwk9@naver.com>
2025-05-12 14:41:06 +0900

    [feat] 치트키 비활성화
    [feat] 치트키 비활성화

--- Assets/01.Scripts/Component/CheatController.cs
+++ Assets/01.Scripts/Component/CheatController.cs
@@ -17,17 +17,17 @@ public class CheatController : MonoBehaviour
         //    Service.SpawnMonster(transform, monster, 5);
         //}
 
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            var spawnPos = this.transform.position;
-            GameManager.effect.Show(spawnPos, "Blood");
-            GameManager.effect.FloorBlood(spawnPos);
-        }
+        //if (Input.GetKeyDown(KeyCode.V))
+        //{
+        //    var spawnPos = this.transform.position;
+        //    GameManager.effect.Show(spawnPos, "Blood");
+        //    GameManager.effect.FloorBlood(spawnPos);
+        //}
 
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            var spawnPos = this.transform.position;
-            GameManager.effect.Damage(spawnPos, 1512);
-        }
+        //if (Input.GetKeyDown(KeyCode.B))
+        //{
+        //    var spawnPos = this.transform.position;
+        //    GameManager.effect.Damage(spawnPos, 1512);
+        //}
     }
 }
