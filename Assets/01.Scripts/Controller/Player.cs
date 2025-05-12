aa0728a5d0cc3a93e31a75adc88df15c2eb70972
kim JY <131152254+GUYoof@users.noreply.github.com>
2025-05-12 15:21:32 +0900

    feat : 데코추가, 층으로 이동하는 코드 추가
    feat : 데코추가, 층으로 이동하는 코드 추가

--- Assets/01.Scripts/Controller/Player.cs
+++ Assets/01.Scripts/Controller/Player.cs
@@ -175,6 +175,7 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy")) inRange = true;
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
