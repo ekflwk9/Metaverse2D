424b5f3fb527de502966cdef8ba6e50356ead218
ekflwk9 <ekflwk9@naver.com>
2025-05-12 15:22:19 +0900

    [feat] 종혁님 브렌치 합침
    [feat] 종혁님 브렌치 합침

--- Assets/01.Scripts/Component/LoadComponent.cs
+++ Assets/01.Scripts/Component/LoadComponent.cs
@@ -8,6 +8,6 @@ public class LoadComponent : MonoBehaviour
         //Loading씬 모든 데이터 할당 후 게임 시작
         GameManager.effect.Load();
         GameManager.sound.Load();
-        //SceneManager.LoadScene("Start");
+        SceneManager.LoadScene("Start");
     }
 }
