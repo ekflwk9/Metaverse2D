2e03eb60e2504e6db86fe92ca39a85ca1bc939b8
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-05-13 17:27:39 +0900

    feat : UiManager.cs
    UI 개선
    UI 코드 개선
    UI 기능 수정

--- Assets/01.Scripts/Logic/UiManager.cs
+++ Assets/01.Scripts/Logic/UiManager.cs
@@ -1,13 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class UiManager : MonoBehaviour
 {
     public Serialized serialized;
     [SerializeField] public Image healthBarImg;
+
+    [SerializeField] private Sprite videoSprite;
+    [SerializeField] private Sprite audioSprite;
+    [SerializeField] private Sprite gameSprite;
+    [SerializeField] private Sprite videoSelectSprite;
+    [SerializeField] private Sprite audioSelectSprite;
+    [SerializeField] private Sprite gameSelectSprite;
+    [SerializeField] private Image video;
+    [SerializeField] private Image audio;
+    [SerializeField] private Image game;
+
+    public void VideoBtn()
+    {
+        if (video.sprite == videoSprite)
+        {
+            video.sprite = videoSelectSprite;
+            audio.sprite = audioSprite;
+            game.sprite = gameSprite;
+        }
+    }
+
+    public void AudioBtn()
+    {
+        if (audio.sprite == audioSprite)
+        {
+            audio.sprite = audioSelectSprite;
+            video.sprite = videoSprite;
+            game.sprite = gameSprite;
+        }
+    }
+
+    public void GameBtn()
+    {
+        if (game.sprite == gameSprite)
+        {
+            game.sprite = gameSelectSprite;
+            video.sprite = videoSprite;
+            audio.sprite = audioSprite;
+        }
+    }
+
 
     public void MainUiAllToggle()
     {
@@ -25,7 +63,7 @@ public class UiManager : MonoBehaviour
     public void GameStart()
     {
         MainUiAllToggle();
-        SceneManager.LoadScene("DungeonStart");
+        GameManager.ChangeScene("DungeonStart");
     }
 
     public void GameExit()
@@ -36,13 +74,13 @@ public class UiManager : MonoBehaviour
         Application.Quit();
     }
 
-    public void SettingScreen()
+    public void SettingToggle()
     {
         MainUiAllToggle();
-        SettingToggle();
+        SettingScreen();
     }
 
-    public void SettingToggle()
+    public void SettingScreen()
     {
         bool isSettings = serialized.settingUi.activeSelf;
         if (isSettings)
