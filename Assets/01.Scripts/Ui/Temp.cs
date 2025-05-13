abd3e01cfe09bd986f917baad9e55a0ad0d81537
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-05-13 13:56:41 +0900

    fix : Resource, UI Script, SkillCard
    사용되지 않는 리소스 제거
    UI 관련 스크립트 수정
    스킬 카드 내용 추가

--- Assets/01.Scripts/Ui/Temp.cs
+++ Assets/01.Scripts/Ui/Temp.cs
@@ -10,29 +10,8 @@ public class Temp : MonoBehaviour
 
     [SerializeField] private string str;
 
-    //public bool isCheat = false;
-
     public void SkillCard()
     {
         GameObject SkillPrefabs = Instantiate(skillCardPrefab, skillCardParent);
     }
-
-    //private void Update()
-    //{
-    //    if (str != "WASDWSAD")
-    //    {
-    //        if (Input.GetKeyDown(KeyCode.W)) str += "W";
-    //        if (Input.GetKeyDown(KeyCode.A)) str += "A";
-    //        if (Input.GetKeyDown(KeyCode.S)) str += "S";
-    //        if (Input.GetKeyDown(KeyCode.D)) str += "D";
-    //    }
-    //    else
-    //    {
-    //        isCheat = true;
-    //    }
-
-    //    if (isCheat)
-    //    {
-    //    }
-    //}
 }
