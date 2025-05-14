b009a30c71aea3b1d6dc5653752d75300887aeb6
ekflwk9 <ekflwk9@naver.com>
2025-05-14 22:19:03 +0900

    [feat] 다혜님 브렌치 합체 준비
    [feat] 다혜님 브렌치 합체 준비

--- Assets/01.Scripts/Logic/Service.cs
+++ Assets/01.Scripts/Logic/Service.cs
@@ -89,6 +89,8 @@ public class Service
             monsterPos.y = Random.Range(0, 8);
 
             var spawnMonster = MonoBehaviour.Instantiate(monster[ranMonsterType]);
+            spawnMonster.name = $"{monster[ranMonsterType].name}_{i}";
+
             spawnMonster.transform.position = _mapPos.transform.position + monsterPos;
             spawnMonster.gameObject.SetActive(true);
         }
