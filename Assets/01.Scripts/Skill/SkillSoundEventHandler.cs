f267d5a68de8229072f8e0eb325c43c1c43dd776
LeeJongHyuk <jjong7477@gmail.com>
2025-05-14 16:26:37 +0900

    [fix] SoundResource Location Changed

---
+++ Assets/01.Scripts/Skill/SkillSoundEventHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillSoundEventHandler : MonoBehaviour
+{
+    public string[] soundName;
+
+    public void PlaySound()
+    {
+        var randomSound = Random.Range(0, soundName.Length);
+        GameManager.sound.OnEffect(soundName[randomSound]);
+    }
+}
