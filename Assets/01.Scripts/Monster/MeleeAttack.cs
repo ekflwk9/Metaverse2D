1c1240aa376a781a170ba26ceccbb007fcdb1c41
ekflwk9 <ekflwk9@naver.com>
2025-05-14 22:24:13 +0900

    Revert "Merge remote-tracking branch 'origin/Dahye_03' into Jang_6"
    This reverts commit 422339b2428895f1f7af1ce9381e2ea59909a923, reversing
    changes made to b009a30c71aea3b1d6dc5653752d75300887aeb6.

--- Assets/01.Scripts/Monster/MeleeAttack.cs
+++ Assets/01.Scripts/Monster/MeleeAttack.cs
@@ -4,16 +4,7 @@ public class MeleeAttack : MonsterAttackBase
 {
     protected override void DoAttack()
     {
-        float distanceToPlayer = Vector2.Distance(transform.position, GameManager.player.transform.position);
-
-        if (distanceToPlayer <= monsterBase.attackRange)
-        {
-            GameManager.player.OnHit(attackDamage);
-        }
-        else
-        {
-            Service.Log($"공격 실패 : 범위 밖");
-        }
+         GameManager.player.OnHit(attackDamage);
     }
 
     public void TriggerAttack()
