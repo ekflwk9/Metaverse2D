93924b72a46723db8f3da50ea26512a05e971077
ekflwk9 <ekflwk9@naver.com>
2025-05-14 22:24:05 +0900

    Reapply "Merge remote-tracking branch 'origin/Dahye_03' into Jang_6"
    This reverts commit cc1aa97dec2f44c5e09e2c6c278091a6921659ae.

--- Assets/01.Scripts/Monster/MeleeAttack.cs
+++ Assets/01.Scripts/Monster/MeleeAttack.cs
@@ -4,7 +4,16 @@ public class MeleeAttack : MonsterAttackBase
 {
     protected override void DoAttack()
     {
-         GameManager.player.OnHit(attackDamage);
+        float distanceToPlayer = Vector2.Distance(transform.position, GameManager.player.transform.position);
+
+        if (distanceToPlayer <= monsterBase.attackRange)
+        {
+            GameManager.player.OnHit(attackDamage);
+        }
+        else
+        {
+            Service.Log($"공격 실패 : 범위 밖");
+        }
     }
 
     public void TriggerAttack()
