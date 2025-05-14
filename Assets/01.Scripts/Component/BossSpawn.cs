373295bbb343584dc65f019bb4e5d68f52c39ac8
LeeJongHyuk <jjong7477@gmail.com>
2025-05-15 03:18:48 +0900

    [feat] BossSpawn 스크립트 추가

---
+++ Assets/01.Scripts/Component/BossSpawn.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawn : MonoBehaviour,
+IItemEnter
+{
+    private GameObject itemInfo;
+
+    private void Awake()
+    {
+        itemInfo = Service.FindChild(this.transform, "ItemInfo").gameObject;
+        GameManager.SetComponent(this);
+    }
+
+    public void OnItem()
+    {
+        this.gameObject.SetActive(false);
+
+        var bossResource = Service.FindResource("Enemy", "Boss");
+        var boss = Instantiate(bossResource);
+
+        boss.name = bossResource.name;
+
+        boss.GetComponent<MonsterBase>().SetMonster();
+
+        boss.transform.position = this.transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) itemInfo.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) itemInfo.SetActive(false);
+    }
+}
