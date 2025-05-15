b6df8d7d6cef960d1ae0c6b3e027c059a211e465
ekflwk9 <ekflwk9@naver.com>
2025-05-15 09:26:10 +0900

    [feat] 보스 히트 버그 수정
    [feat] 보스 히트 버그 수정

--- Assets/01.Scripts/Monster/BringerOfDeath.cs
+++ Assets/01.Scripts/Monster/BringerOfDeath.cs
@@ -6,7 +6,7 @@ public class BringerOfDeath : Monster
 {
     private void Attack()
     {
-        if (Service.Distance(target.position, transform.position) < 1.8f)
+        if (Service.Distance(target.position, transform.position) < 2.3f)
         {
             GameManager.gameEvent.Hit("Player", dmg);
         }
